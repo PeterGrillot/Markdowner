0e9fcb0054478b680ba9d3e2ab7e323fbf2680b6
petergrillot <petergrillot@gmail.com>
2017-11-09 18:07:20 -0500

    adding redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import { creatStore } from 'redux'
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
