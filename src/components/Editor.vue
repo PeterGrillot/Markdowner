c223c21d029aa6400fe036a25ba1a25b6a2ff728
Peter Grillot <petergrillot@gmail.com>
2017-11-21 16:36:58 -0500

    editing save and adding counter

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,20 +1,22 @@
 <template>
-	<div id="getNote">
-		<div class="user-input">
-			<textarea ref="userInput" class="user-input__textarea"
-								:value="newNote"
-								@keyup="getNote"
-								placeholder="Markdown! Start Typing"></textarea>
-			<div class="button__group">
+	<div id="Editor">
+		<div class="button__group">
 				<button class="button"
 								@click="startNote"><i class="icon ion-document"></i> New</button>
 				<button class="button"
 								@click="saveNote"><i class="icon ion-archive"></i> Save</button>
 				<button class="button"
-								@click="openMenu"><i class="icon ion-folder"></i> Notes</button>
+								@click="openMenu"><i class="icon ion-folder"></i> Notes ({{notes.length}})</button>
 			</div>
+		<div class="user-input">
+			<textarea ref="userInput" class="user-input__textarea"
+								:value="newNote"
+								@keyup="getNote"
+								@focus="closeMenu"
+								placeholder="Markdown! Start Typing">
+			</textarea>
 		</div>
-		<div class="markdown-output" ref="output"></div>	
+		<div class="markdown-output" ref="output"></div>
 	</div>
 </template>
 
@@ -58,22 +60,43 @@ export default {
 			}
 			toolbarOpen = !toolbarOpen
 		},
+		closeMenu(event){
+			const toobarEl = document.getElementById('currentNotes')
+			if (toolbarOpen === true){
+				event.target.removeAttribute('data-open')
+				toobarEl.removeAttribute('data-open')
+				toolbarOpen = !toolbarOpen
+			}
+		},
 	},
 	computed: {
 		newNote() {
 			return this.$store.getters.newNote
+		},
+		notes() {
+			return this.$store.getters.notes
 		}
 	},
 }
 
 </script>
-<style scoped>
+<style>
 .user-input{
 	display: flex;
 	position: relative;
 }
 .markdown-output{
 	padding: 0 6px;
+	font-family: 'Open Sans', sans-serif;
+	overflow-y: scroll;
+}
+.markdown-output h1,
+.markdown-output h2,
+.markdown-output h3,
+.markdown-output h4,
+.markdown-output h5,
+.markdown-output h6{
+	font-family: 'Oswald', sans-serif;
 }
 .markdown-output,
 .user-input{
@@ -89,6 +112,7 @@ export default {
 	background-color: #eee;
 	outline: none;
 	padding: 6px;
+	overflow-y: scroll;
 }
 code {
 	background: #f4f5f6;
