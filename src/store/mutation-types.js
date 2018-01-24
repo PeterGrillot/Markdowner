6cb7c8289b81ff27b6406769fc0da9cfa5489754
Peter Grillot <petergrillot@gmail.com>
2018-01-24 14:20:02 -0500

    reorganized store folder

---
+++ src/store/mutation-types.js
@@ -0,0 +1,7 @@
+export const MENU_STATUS = 'MENU_STATUS'
+export const GET_NOTE = 'GET_NOTE'
+export const ADD_NOTE = 'ADD_NOTE'
+export const EDIT_NOTE = 'EDIT_NOTE'
+export const SAVE_NOTE = 'SAVE_NOTE'
+export const REMOVE_NOTE = 'REMOVE_NOTE'
+export const CLEAR_NOTE = 'CLEAR_NOTE'
---
+++ src/store/mutations.js
@@ -0,0 +1,34 @@
+import Vue from 'vue'
+import * as types from './mutation-types'
+
+export default {
+	[types.MENU_STATUS](state) {
+		state.status = !state.status
+		console.log(`state: ${state.status}`)
+	},
+	[types.GET_NOTE](state, note) {
+		state.newNote = note
+	},
+	[types.ADD_NOTE](state) {
+		state.notes.push({
+			body: state.newNote,
+			completed: false
+		})
+	},
+	[types.EDIT_NOTE](state, note) {
+		var notes = state.notes
+		notes.splice(notes.indexOf(note), 1)
+		state.notes = notes
+		state.newNote = note.body
+	},
+	[types.SAVE_NOTE](state, note) {
+
+	},
+	[types.REMOVE_NOTE](state, note) {
+		var notes = state.notes
+		notes.splice(notes.indexOf(note), 1)
+	},
+	[types.CLEAR_NOTE](state) {
+		state.newNote = ''
+	}
+}
